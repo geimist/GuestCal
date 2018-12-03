79a4f998d5663f599e6d4af1f6e8334d9ce3287d
Stephan Geisler <synotr@geimist.eu>
2018-12-03 12:48:09 +0000

    Dateien hochladen nach „guestcal/admin/templates/default“

---
+++ guestcal/admin/templates/default/foot.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file is part of GuestCal: http://guestcal.sourceforge.net/
+ *
+ * ©2012 dotplex e.K. / www.dotplex.de / info@dotplex.de
+ * Licenced under GPLv3 Open Source Licence: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+echo '</body>';
+echo '</html>';
---
+++ guestcal/admin/templates/default/head.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of GuestCal: http://guestcal.sourceforge.net/
+ *
+ * ©2012 dotplex e.K. / www.dotplex.de / info@dotplex.de
+ * Licenced under GPLv3 Open Source Licence: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+echo '<html xmlns="http://www.w3.org/1999/xhtml">';
+echo '<!-- GuestCal ' . VERSION . ' - http://www.guestcal.com/ -->';
+echo '<head>';
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+echo '<meta http-equiv="Content-Language" content="' . LANG_ABBR . '" />';
+echo '<meta name="robots" content="noindex, follow" />';
+echo '<link rel="stylesheet" href="' . $templateAdminPath . 'main.css" charset="utf-8" />';
+echo '<link rel="stylesheet" href="' . BASEDIR . 'css.php" charset="utf-8" />';
+echo '<title>GuestCal - ' . $pageTitle . '</title>';
+echo '</head>';
+echo '<body>';
